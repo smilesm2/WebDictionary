979c9d4eba4986ae70deeaeb112e318f3c2356bb
smilesm2 <smile_sm2@hotmail.com.tw>
2014-12-20 03:39:33 +0800

    create file
    create file

---
+++ WebDictionary/myscript.js
@@ -0,0 +1,23 @@
+﻿$(function(){
+    $(document.body).bind('mouseup', function(e){
+        var selection;
+        
+        if (window.getSelection) {
+          selection = window.getSelection();
+        } else if (document.selection) {
+          selection = document.selection.createRange();
+        }
+        $("#meaning")
+			.html(selection.toString())
+			.css(
+				{
+					"top":e.pageY+10,
+					"left":e.pageX,
+					"display":"block"			
+				}
+			)
+        // !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
+    });
+	$("body").append("<div id='meaning' style='display:none;position:absolute;z-index:999'></div>");
+
+});
